2d927bff1a1f8468fac3dfec29debe1f6a942155
veronicaambassador <icha_fortminor@yahoo.com>
2017-04-17 06:05:05 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,9 +7,7 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
-	foreach ($events['events'] as $event) {
-		
-print $even;
+	foreach ($events['events'] as $event) {		
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
