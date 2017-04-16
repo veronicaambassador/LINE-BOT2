a6f49eb2c44d40caab16ba73779fa2126a8f1ff4
veronicaambassador <icha_fortminor@yahoo.com>
2017-04-16 21:03:45 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,14 +25,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $nama
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$nama],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -50,4 +50,5 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK"; 
+echo "OK";
+echo $nama;
