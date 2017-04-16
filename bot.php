b3a6ca7bd5e02715c414f8979b5d2a95c675c549
veronicaambassador <icha_fortminor@yahoo.com>
2017-04-16 20:17:48 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,8 +11,8 @@ if (!is_null($events['events'])) {
 
 }
 
-echo ('<pre> print the json ');
-    print_r ($events);
-    echo ('</pre>');
+$data = json_decode(file_get_contents('php://input'), true);
+print_r($data);
+echo $data;
 
-    echo '<br>output:</br>';
+echo file_get_contents('php://input');
