6fdcf1a71a78c101f0df991dc19dd7ff6602ad73
veronicaambassador <icha_fortminor@yahoo.com>
2017-04-17 08:01:07 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = '1rtphuwCd07x26RycF7MsBcvjlWeDzMB1NTlkVoh4+mS7r4sbn++VqxQu4r8VwQzQxYmHqRZt5plxmjgCutvZZXAE47GOcH5QSmVrHgs0fF8gfNQHQDq/apLXuxR/7FsnaIdHR4CryyTC6lDpeEGOQdB04t89/1O/w1cDnyilFU=';
 
 
-$host = 'localhost:8080'; 
+$host = 'localhost'; 
 $user = 'root'; 
 $password = '';
 $dbname = 'db_polling';
